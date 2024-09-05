630b86e0b2574485fa5f3af52859ea0449d5b8f0
Carlos <42675953+carlossr97@users.noreply.github.com>
2024-09-05 01:20:45 -0300

    Delete plugins directory

--- plugins/cordova-plugin-app-exit/src/browser/ExitApp.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  exitApp: function () {
-    alert('App will be reload');
-    window.location.reload();
-  }
-};
-
-require('cordova/exec/proxy').add('ExitApp', module.exports);
